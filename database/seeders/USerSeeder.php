05698385f9bba4cce1c5e05e0d5403fcf5b0156f
Rey Anthony Mapalad <ramapalad@gmail.com>
2025-09-23 11:17:17 +0800

    Added UserSedder.php

--- database/seeders/USerSeeder.php
+++ database/seeders/USerSeeder.php
@@ -12,6 +12,25 @@ class USerSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'), // Hash the password
+            'role' => UserRoleEnum::SUPER_ADMIN, // Assign Super Admin role
+        ]);
+        // Inventory Manager User
+        User::create([
+            'name' => 'Manager User',
+            'email' => 'manager@example.com',
+            'password' => Hash::make('password'),
+            'role' => UserRoleEnum::INVENTORY_MANAGER, // Assign Inventory Manager role
+        ]);
+        // Regular Inventory User
+        User::create([
+            'name' => 'Basic User',
+            'email' => 'user@example.com',
+            'password' => Hash::make('password'),
+            'role' => UserRoleEnum::INVENTORY_USER, // Assign Inventory User role
+        ]);
     }
 }
