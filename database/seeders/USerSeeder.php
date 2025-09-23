2ae7fb238c006960c7aeb73dee4b6814ec88b134
Rey Anthony Mapalad <ramapalad@gmail.com>
2025-09-23 11:21:32 +0800

    Edited UserSeeder.ph

--- database/seeders/USerSeeder.php
+++ database/seeders/USerSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use App\UserRoleEnum;
 
 class USerSeeder extends Seeder
 {
