7c6be4de104110da6020df08ad2a5e369b63815d
Rey Anthony Mapalad <ramapalad@gmail.com>
2025-09-23 13:21:37 +0800

    created app/Models Manufacturer.php

---
+++ app/Models/Manufacturer.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Manufacturer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'url',
+        'support_url',
+        'support_phone',
+        'support_email',
+    ];
+
+    /**
+     * Get the assets for the manufacturer.
+     */
+    public function assets(): HasMany
+    {
+        return $this->hasMany(Asset::class);
+    }
+}
