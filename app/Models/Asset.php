71e3b9c9ff1e85e72e5b2c0a554e9b17533a01a7
Rey Anthony Mapalad <ramapalad@gmail.com>
2025-09-23 13:26:55 +0800

    created app/Models/Asset.php

---
+++ app/Models/Asset.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Enum\AssetStatusEnum;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Asset extends Model
+{
+    protected $fillable = [
+        'name',
+        'serial_number',
+        'asset_tag',
+        'category_id',
+        'location_id',
+        'manufacturer_id',
+        'model',
+        'purchase_date',
+        'warranty_expiration',
+        'status',
+        'assigned_to_user_id',
+        'notes',
+    ];
+
+    protected $casts = [
+        'purchase_date' => 'date',
+        'purchase_price' => 'decimal:2',
+        'status' => AssetStatusEnum::class,
+    ];
+}
